1502b1a6402254657834831d8290588f620ef4ea
Pradyu <prxdyu@gmail.com>
2023-04-25 12:55:22 +0530

    Changed everything back

--- app.py
+++ app.py
@@ -35,7 +35,7 @@ gravatar = Gravatar(app,
                     base_url=None)
 ##CONNECT TO DB
 app.config['SQLALCHEMY_ENGINE_OPTIONS'] = {"pool_pre_ping": True}
-app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("DATABASE2_URL")
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("DATABASE_URL")
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
